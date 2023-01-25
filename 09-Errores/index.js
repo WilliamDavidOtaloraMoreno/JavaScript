efb0e84dac57a3bb54d0788922a62b8944d39ebd
William David Otálora Moreno <williamotalora22@gmail.com>
2023-01-25 15:27:10 -0500

    Errores

---
+++ 09-Errores/index.js
@@ -0,0 +1,7 @@
+const logger = require('./logger');
+
+//logger.log("Hola estoy saliendo por pantalla")
+logger.info("Hola esto es un mensaje informativo")
+logger.debug("Hola esto es un mensaje de debug")
+logger.warn("Hola esto es un mensaje de advertencia")
+logger.error("Hola esto es un mensaje de error")
